400d93e2c82f6ef64d7f0b523303e418e4025b26
Char Howland <60200853+cjhowland@users.noreply.github.com>
2021-04-21 17:55:22 -0700

    Add files via upload
    I added a subclass inheriting from the Message class that focuses on converting plaintext messages into encrypted messages and easily modifying the content.

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -87,10 +87,97 @@ class Message(object):
 
 
 
+
+class PlainMsg(Message):
+    '''
+    This subclass focuses on converting plaintext messages into encrypted
+    messages and easily modifying the content.
+
+    Example Usage:
+    >>> msg = PlainMsg('Hello, World!', 5)
+    >>> print(msg.get_encrypted_msg())
+    # 'Mjqqt, Btwqi!'
+    >>> msg.change_shift(23)
+    >>> print(msg.get_encrypted_msg())
+    # 'Ebiil, Tloia!'
+    '''
+
+    def __init__(self, text, shift):
+        '''
+        Initializes a PlainMsg type object, inheriting from the Message parent class
+
+        Inputs:
+            - text (string): the plain message text
+            - shift (int): the amount the encrypted message to to be shifted
+
+        A PlainMsg object inherits from Message and has 4 total attributes:
+            - self.message_text (string, determined by input)
+            - self.shift (integer, determined by input)
+            - self.shift_dict (dictionary, created using shift)
+            - self.encrypted_msg (string, created using shift)
+        '''
+        Message.__init__(self,text)
+        self.message_text = text
+        self.shift = shift
+        self.shift_dict = Message.build_shift_dict(self,shift)
+        self.encrypted_msg = Message.apply_shift(self,shift)
+
+    def get_shift(self):
+        ''' 
+        Safely access the self.shift attribute outside the class.
+
+        Outputs:
+            - (integer): the value of self.shift
+        '''
+        return self.shift
+
+    def change_shift(self, new_shift):
+        '''
+        Changes self.shift of the PlainMsg object and updates all other attributes which depend on self.shift.
+
+        Input:
+            - new_shift (integer): new value to shift all the letters by
+        Output:
+            - Nothing
+        '''
+        self.shift = new_shift
+        self.shift_dict = Message.build_shift_dict(self,self.shift)
+        self.encrypted_msg = Message.apply_shift(self,self.shift)
+
+    def get_encrypted_msg(self):
+        '''
+        Safely acces the self.encrypted_msg attribute outside the class
+
+        Outputs:
+            - (string): the value of self.encrypted_msg
+        '''
+        return self.encrypted_msg
+
+
+
+
+
+
+
+
+
 # Testing
 if __name__ == '__main__':
+    # test of the Message class
     plaintext = Message('Happy Birthday!')
     print(plaintext.get_message_text())
+    # 'Happy Birthday!'
     print(plaintext.apply_shift(1))
+    # 'Ibqqz Cjsuiebz!'
     print(plaintext.apply_shift(18))
+    # 'Zshhq Tajlzvsq!'
+
+    # test of the PlainMsg class
+    msg = PlainMsg('Hello, World!', 5)
+    print(msg.get_encrypted_msg())
+    # 'Mjqqt, Btwqi!'
+    msg.change_shift(23)
+    print(msg.get_encrypted_msg())
+    # 'Ebiil, Tloia!'
+    
 
